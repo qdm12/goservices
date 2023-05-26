2bdaa0578be4c88fd40b1b50133463c2031fec78
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-05-26 12:23:00 +0000

    change(hooks/log): change info logs to debug logs

--- hooks/log.go
+++ hooks/log.go
@@ -5,7 +5,7 @@ type LogHooks struct {
 }
 
 type Logger interface {
-	Info(s string)
+	Debug(s string)
 	Warn(s string)
 }
 
@@ -16,26 +16,26 @@ func NewWithLog(logger Logger) *LogHooks {
 }
 
 func (h *LogHooks) OnStart(service string) {
-	h.logger.Info(service + " starting")
+	h.logger.Debug(service + " starting")
 }
 
 func (h *LogHooks) OnStarted(service string, err error) {
 	if err != nil {
 		h.logger.Warn("starting " + service + ": " + err.Error())
 	} else {
-		h.logger.Info(service + " started")
+		h.logger.Debug(service + " started")
 	}
 }
 
 func (h *LogHooks) OnStop(service string) {
-	h.logger.Info(service + " stopping")
+	h.logger.Debug(service + " stopping")
 }
 
 func (h *LogHooks) OnStopped(service string, err error) {
 	if err != nil {
 		h.logger.Warn("stopping " + service + ": " + err.Error())
 	} else {
-		h.logger.Info(service + " stopped")
+		h.logger.Debug(service + " stopped")
 	}
 }
 
@@ -43,6 +43,6 @@ func (h *LogHooks) OnCrash(service string, err error) {
 	if err != nil {
 		h.logger.Warn(service + " crashed: " + err.Error())
 	} else {
-		h.logger.Info(service + " crashed")
+		h.logger.Debug(service + " crashed")
 	}
 }
